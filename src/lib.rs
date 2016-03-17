0a96a38b7a34a8b260931b9f11182c3b0e9753db
Matt Friedman <matt@friedm.com>
2016-03-16 19:32:04 -0500

    add simple regex api

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,23 @@
 #[derive(PartialEq,Debug)]
-struct Regex;
-
-impl Regex {
-    pub fn new() -> Regex {
-        Regex
-    }
-
-    pub fn is_match(&self) -> bool {
-        true
-    }
+struct Regex {
+    pattern: String
 }
 
+impl Regex {
+    pub fn from(pattern: &str) -> Regex {
+        Regex {
+            pattern: pattern.to_owned()
+        }
+    }
 
+    pub fn is_match(&self, text: &str) -> bool {
+        true
+    }
+
+    pub fn find_first(&self, text: &str) -> (usize, usize) {
+        (1, 2)
+    }
+}
 
 #[cfg(test)]
 mod regex_spec {
@@ -19,12 +25,19 @@ mod regex_spec {
 
     #[test]
     fn creates_new() {
-        assert_eq!(Regex, Regex::new());
+        assert_eq!(Regex{
+            pattern: String::from(r"pattern")
+        }, Regex::from(r"pattern"));
     }
 
     #[test]
     fn matches() {
-        let regex = Regex::new();
-        assert_eq!(true, regex.is_match());
+        let regex = Regex::from(r"a");
+        assert_eq!(true, regex.is_match(r"baaa"));
+    }
+
+    #[test]
+    fn find() {
+        assert_eq!((1, 2), Regex::from(r"a").find_first("baaa"))
     }
 }
