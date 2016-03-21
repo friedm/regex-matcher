4bd205b92090848e3ab9a245c1de39615791da82
Matt Friedman <matt@friedm.com>
2016-03-21 12:40:07 -0500

    immediately apply postfix regex operators

--- src/nfa/mod.rs
+++ src/nfa/mod.rs
@@ -9,8 +9,8 @@ enum Expr {
     Sequence(Box<Expr>, Box<Expr>),
     Or(Box<Expr>, Box<Expr>),
     Optional(Box<Expr>),
-    KleeneStar(Box<Expr>),
-    KleenePlus(Box<Expr>)
+    ZeroOrMore(Box<Expr>),
+    OneOrMore(Box<Expr>)
 }
 
 impl FromStr for Expr {
@@ -22,17 +22,17 @@ impl FromStr for Expr {
         let mut operator_stack = Vec::<char>::new();
 
         for c in s.chars() {
-            if operators.contains(&c) {
-                pop_operator(&mut operator_stack, &mut output_queue);
+            if binary_operators.contains(&c) {
+                pop_infix_operators(&mut operator_stack, &mut output_queue);
                 operator_stack.push(c);
+            } else if unary_postfix_operators.contains(&c) {
+                apply_postfix_operator(c, &mut output_queue);
             } else {
                 output_queue.push_back(Expr::Single(c))
             }
         }
 
-        while !operator_stack.is_empty() {
-            pop_operator(&mut operator_stack, &mut output_queue);
-        }
+        pop_infix_operators(&mut operator_stack, &mut output_queue);
 
         // build sequence tree from queue
         while output_queue.len() > 1 {
@@ -47,26 +47,51 @@ impl FromStr for Expr {
     }
 }
 
-static operators: &'static [char] = &['|', '?'];
+static unary_postfix_operators: &'static [char] = &['?', '*', '+'];
+static binary_operators: &'static [char] = &['|'];
 
-fn pop_operator(operator_stack: &mut Vec<char>, output_queue: &mut VecDeque<Expr>) {
+fn pop_infix_operators(operator_stack: &mut Vec<char>, output_queue: &mut VecDeque<Expr>) {
     while !operator_stack.is_empty() {
+
+        println!("{:?}, {:?}", output_queue, operator_stack);
         // TODO handle prescedence
         match operator_stack.pop().unwrap() {
             '|' => { 
-                let right = output_queue.pop_back().unwrap();
-                let left = output_queue.pop_back().unwrap();
-                output_queue.push_back(
-                    Expr::Or(Box::new(left), 
-                             Box::new(right)));
+                apply_binary_operator(output_queue, &|l,r| Expr::Or(l,r));
             },
-            '?' => {
-                let item = output_queue.pop_back().unwrap();
-                output_queue.push_back(
-                    Expr::Optional(Box::new(item)));
-            },
-            _ => panic!("unknown operator")
+            _ => panic!("unknown infix operator")
         }
     }
+}
+
+fn apply_postfix_operator(operator: char, output_queue: &mut VecDeque<Expr>) {
+    match operator {
+        '?' => {
+            apply_unary_operator(output_queue, &|expr| Expr::Optional(expr));
+        },
+        '*' => {
+            apply_unary_operator(output_queue, &|expr| Expr::ZeroOrMore(expr));
+        },
+        '+' => {
+            apply_unary_operator(output_queue, &|expr| Expr::OneOrMore(expr));
+        }
+        _ => panic!("unknown postfix operator")
+    }
+}
+
+fn apply_binary_operator(output_queue: &mut VecDeque<Expr>, 
+                         constructor: &Fn(Box<Expr>, Box<Expr>) -> Expr) {
+
+    let right = output_queue.pop_back().unwrap();
+    let left = output_queue.pop_back().unwrap();
+
+    output_queue.push_back(constructor(Box::new(left),Box::new(right)));
+}
+
+fn apply_unary_operator(output_queue: &mut VecDeque<Expr>,
+                        constructor: &Fn(Box<Expr>) -> Expr) {
+
+    let item = output_queue.pop_back().unwrap();
+    output_queue.push_back(constructor(Box::new(item)));
 }
 
--- src/nfa/spec.rs
+++ src/nfa/spec.rs
@@ -34,3 +34,39 @@ fn parse_optional() {
     "a?".parse::<Expr>().unwrap());
 }
 
+#[test]
+fn parse_kleene_star() {
+    assert_eq!(Expr::ZeroOrMore(Box::new(Expr::Single('a'))),
+    "a*".parse::<Expr>().unwrap());
+}
+
+#[test]
+fn parse_kleene_plus() {
+    assert_eq!(Expr::OneOrMore(Box::new(Expr::Single('a'))),
+    "a+".parse::<Expr>().unwrap());
+}
+
+#[test]
+fn parse_combination() {
+    assert_eq!(
+        Expr::Sequence(Box::new(Expr::Single('a')),
+                       Box::new(Expr::Sequence(
+                               Box::new(Expr::OneOrMore(Box::new(Expr::Single('b')))),
+                               Box::new(Expr::Single('c'))))),
+               "ab+c".parse::<Expr>().unwrap());
+}
+
+#[test]
+fn parse_parens() {
+    assert_eq!(Expr::OneOrMore(Box::new(Expr::Sequence(Box::new(Expr::Single('a')),
+                                             Box::new(Expr::Single('b'))))),
+               "(ab)+".parse::<Expr>().unwrap());
+
+    assert_eq!(
+        Expr::Sequence(
+            Box::new(Expr::Optional(Box::new(Expr::Sequence(Box::new(Expr::Single('a')),
+                                          Box::new(Expr::Single('b')))))),
+            Box::new(Expr::Single('c'))),
+               "(ab)+c".parse::<Expr>().unwrap());
+}
+
