c0d07b6f5ddb0895184fe062860e91b25a66178f
Matt Friedman <matt@friedm.com>
2016-03-30 12:27:59 -0500

    build option NFA

--- src/nfa/mod.rs
+++ src/nfa/mod.rs
@@ -5,8 +5,8 @@ use ::expr::Expr;
 
 #[derive(PartialEq,Debug,Clone)]
 enum State {
-    State{edge: char, out: Edge},
-    Split{s1: char, out1: Edge, s2: char, out2: Edge}
+    State{edge: Option<char>, out: Edge},
+    Split{s1: Option<char>, out1: Edge, s2: Option<char>, out2: Edge}
 }
 
 #[derive(PartialEq,Debug,Clone)]
@@ -17,11 +17,11 @@ enum Edge {
 }
 
 impl State {
-    pub fn state(edge: char, out: Edge) -> State {
+    pub fn state(edge: Option<char>, out: Edge) -> State {
         State::State{edge: edge, out: out}
     }
 
-    pub fn split(s1: char, out1: Edge, s2: char, out2: Edge) -> State {
+    pub fn split(s1: Option<char>, out1: Edge, s2: Option<char>, out2: Edge) -> State {
         State::Split{s1: s1,
                      out1: out1,
                      s2: s2,
@@ -54,16 +54,22 @@ impl NFA {
         let id = match expr {
             &Expr::Single(c) => {
                 let new_state_id = self.states.len();
-                let s = State::state(c, Edge::Detached);
+                let s = State::state(Some(c), Edge::Detached);
                 self.states.push(s);
                 self.states.len() - 1
             },
             &Expr::Sequence(ref a, ref b) => {
-                let left_id = self.build_expr(&*a);
-                let right_id = self.build_expr(&*b);
+                let left_id = self.build_expr(a);
+                let right_id = self.build_expr(b);
                 self.update_outputs(left_id, Edge::Id(right_id));
                 left_id
             },
+            &Expr::Optional(ref expr) => {
+                let expr_id = self.build_expr(expr);
+                let s = State::split(None, Edge::Id(expr_id), None, Edge::Detached);
+                self.states.push(s);
+                self.states.len() - 1
+            }
             _ => panic!()
         };
 
@@ -75,20 +81,30 @@ impl NFA {
         let state = match state {
             State::State{edge, ref out} => {
                 State::state(edge, 
-                             match out {
-                                 &Edge::Detached => {
-                                     new_edge
-                                 },
-                                 &Edge::Id(id) => {
-                                     self.update_outputs(id, new_edge);
-                                     out.clone()
-                                 },
-                                 _ => out.clone()
-                             })
+                             self.replace_edge(out.clone(), new_edge)
+)
             },
-            _ => state
+            State::Split{s1, ref out1, s2, ref out2} => {
+                State::split(s1,
+                             self.replace_edge(out1.clone(), new_edge.clone()),
+                             s2,
+                             self.replace_edge(out2.clone(), new_edge.clone()))
+            }
         };
         self.states[start_id] = state;
+    }
+
+    fn replace_edge(&mut self, edge: Edge, replacement: Edge) -> Edge {
+        match &edge {
+            &Edge::Detached => {
+                replacement
+            },
+            &Edge::Id(id) => {
+                self.update_outputs(id, replacement);
+                edge
+            },
+            _ => edge
+        }
     }
 }
 
--- src/nfa/spec.rs
+++ src/nfa/spec.rs
@@ -5,13 +5,40 @@ use super::{State, Edge, NFA};
 fn build_single() {
     let nfa = NFA::from_expr(&Expr::Single('a'));
 
-    assert_eq!(vec![State::state('a', Edge::End)], nfa.states);
+    assert_eq!(vec![State::state(Some('a'), Edge::End)], nfa.states);
 }
 
 #[test]
 fn build_sequence() {
-    let expr = NFA::from_expr(&Expr::sequence(Expr::Single('a'),Expr::Single('b')));
+    let nfa = NFA::from_expr(&Expr::sequence(Expr::Single('a'),Expr::Single('b')));
     
-    assert_eq!(vec![State::state('a', Edge::Id(1)), State::state('b', Edge::End)], expr.states);
+    assert_eq!(vec![State::state(Some('a'), Edge::Id(1)), State::state(Some('b'), Edge::End)], nfa.states);
 }
+
+#[test]
+fn build_option() {
+    let nfa = NFA::from_expr(&Expr::optional(Expr::Single('a')));
+
+    assert_eq!(vec![State::state(Some('a'), Edge::End), State::split(None, Edge::Id(0), None, Edge::End)],
+        nfa.states);
+}
+
+#[test]
+fn build_complex_option() {
+    let nfa = NFA::from_expr(
+        &Expr::sequence(
+            Expr::optional(Expr::sequence(
+                Expr::Single('a'),
+                Expr::Single('b'))),
+             Expr::Single('a')));
+
+    assert_eq!(vec![
+        State::state(Some('a'), Edge::Id(1)),
+        State::state(Some('b'), Edge::Id(3)),
+        State::split(None, Edge::Id(0),
+                     None, Edge::Id(3)),
+        State::state(Some('a'), Edge::End)
+    ], nfa.states);
+}
+
 
