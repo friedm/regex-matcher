b59d08aa30aa71393c26c769be181c962b3363b8
Matt Friedman <matt@friedm.com>
2016-03-30 11:08:12 -0500

    rustc runs out of memory?

--- src/nfa/mod.rs
+++ src/nfa/mod.rs
@@ -5,54 +5,84 @@ use ::expr::Expr;
 
 #[derive(PartialEq,Clone,Debug)]
 pub enum State {
-    State{edge: char, out: Box<State>},
-    Split{state1: Box<State>, state2: Box<State>},
+    State{edge: char, out: Edge},
+    Split{state1: State, state2: State}
+}
+
+enum Edge {
+    Id(usize),
     Detached,
     End
 }
 
 impl State {
-    pub fn state(edge: char, out: State) -> State {
-        State::State{edge: edge, out: Box::new(out)}
+    pub fn state(edge: char, out: Edge) -> State {
+        State::State{edge: edge, out: out}
     }
 
-    pub fn split(state1: State, state2: State) -> State {
-        State::Split{state1: Box::new(state1),
-                     state2: Box::new(state2)}
+    pub fn split(state1: Edge, state2: Edge) -> State {
+        State::Split{state1: state1,
+                     state2: state2}
+    }
+}
+
+struct NFA {
+    states: Vec<State>
+}
+
+impl NFA {
+
+    pub fn new() -> NFA {
+        NFA {
+            states: Vec::new()
+        }
     }
 
-    pub fn from_expr(expr: &Expr) -> State {
-        let expr = Self::build_expr(expr);
-        expr.with_outputs(State::End)
+    pub fn from_expr(expr: &Expr) -> NFA {
+        let nfa = Self::new();
+
+        let id = nfa.build_expr(expr);
+        nfa.update_outputs(id, State::End);
+        nfa
     }
 
-    fn build_expr(expr: &Expr) -> State {
-        match expr {
+    fn build_expr(&mut self, expr: &Expr) -> usize {
+        let id = match expr {
             &Expr::Single(c) => {
-                State::state(c, State::Detached)
+                let new_state_id = self.states.len();
+                let s = State::state(c, new_state_id + 1);
+                self.states.push(s);
+                self.states.push(State::Detached);
+                self.states.len() - 1
             },
             &Expr::Sequence(ref a, ref b) => {
-                let left = Self::build_expr(&*a);
-                let right = Self::build_expr(&*b);
-                left.with_outputs(right)
+                let left_id = self.build_expr(&*a);
+                let right_id = self.build_expr(&*b);
+                self.update_outputs(left_id, right_id);
+                left_id
             },
             _ => panic!()
-        }
+        };
+
+        id
     }
 
-    fn with_outputs(self, new_state: State) -> Self {
-        match self {
+    fn update_outputs(self, start_id: usize, new_id: usize) {
+        let state = match self.states[start_id] {
             State::State{edge, out} => {
                 State::state(edge, 
-                             match *out {
+                             match self.states[out] {
                                  State::Detached => {
-                                     new_state
+                                     new_id
                                  },
-                                 _ => out.with_outputs(new_state)
+                                 _ => {
+                                     self.update_outputs(out, new_id);
+                                     out
+                                 }
                              })
             },
-            _ => self
-        }
+            _ => self.states[start_id]
+        };
     }
 }
 
--- src/nfa/spec.rs
+++ src/nfa/spec.rs
@@ -5,13 +5,13 @@ use super::{State};
 fn build_single() {
     let nfa = State::from_expr(&Expr::Single('a'));
 
-    assert_eq!(State::state('a', State::End), nfa);
+    assert_eq!(vec![State::state('a', State::End)], nfa);
 }
 
 #[test]
 fn build_sequence() {
     let expr = State::from_expr(&Expr::sequence(Expr::Single('a'),Expr::Single('b')));
     
-    assert_eq!(State::state('a', State::state('b', State::End)), expr);
+    assert_eq!(vec![State::state('a', State::state('b', State::End))], expr);
 }
 
