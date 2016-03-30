e31b1b24e52ace086a36da1ae14f481ea5df20ad
Matt Friedman <matt@friedm.com>
2016-03-30 10:11:09 -0500

    revert some nfa building

--- src/nfa/mod.rs
+++ src/nfa/mod.rs
@@ -2,9 +2,6 @@ use ::expr::Expr;
 
 #[cfg(test)] mod spec;
 
-struct NFA {
-    states: Vec<State>
-}
 
 #[derive(PartialEq,Clone,Debug)]
 pub enum State {
@@ -25,9 +22,8 @@ impl State {
     }
 
     pub fn from_expr(expr: &Expr) -> State {
-
-        let state = Self::build_expr(expr);
-        state.with_outputs(State::End)
+        let expr = Self::build_expr(expr);
+        expr.with_outputs(State::End)
     }
 
     fn build_expr(expr: &Expr) -> State {
@@ -35,63 +31,23 @@ impl State {
             &Expr::Single(c) => {
                 State::state(c, State::Detached)
             },
-            &Expr::Sequence(ref left, ref right) => {
-
-                let left = Self::build_expr(left);
-                let right = Self::build_expr(right);
-
-                left.with_outputs(right)
-            },
-            &Expr::Or(ref left, ref right) => {
-
-                let left = Self::build_expr(left);
-                let right = Self::build_expr(right);
-
-                State::split(left, right)
-            },
-            &Expr::Optional(ref item) => {
-
-                let item = Self::build_expr(item);
-                State::split(item, State::Detached)
-            },
-            //&Expr::ZeroOrMore(ref item) => {
-
-                //let mut split = State::split(State::Detached, State::Detached);
-                //let item = Self::build_expr(item);
-                //let boxed_item = item.with_outputs_to_box(Box::new(split));
-
-                //State::Detached
-
-            //}
             _ => panic!()
         }
     }
 
-    // recursively replace all detached outputs with the given state
-    fn with_outputs(self, new_state: State) -> State {
+    fn with_outputs(self, new_state: State) -> Self {
         match self {
-            State::State{edge, out} => 
-                State::State{edge: edge,  
-                             out: Box::new((*out).with_outputs(new_state.clone()))},
-            State::Split{state1, state2} =>
-                State::Split{state1: Box::new((*state1).with_outputs(new_state.clone())),
-                             state2: Box::new((*state2).with_outputs(new_state.clone()))},
-            State::Detached => new_state,
-            State::End => State::End
+            State::State{edge, out} => {
+                State::state(edge, 
+                             match *out {
+                                 State::Detached => {
+                                     new_state
+                                 },
+                                 _ => *out
+                             })
+            },
+            _ => self
         }
     }
-
-    //fn with_outputs_to_box(self, new_state: Box<State>) -> Box<State> {
-        //match self {
-            //State::State{edge, out} => 
-                //Box::new(State::State{edge: edge,  
-                    //out: (*out).with_outputs_to_box(new_state)}),
-            //State::Split{state1, state2} =>
-                //Box::new(State::Split{state1: (*state1).with_outputs_to_box(new_state),
-                             //state2: (*state2).with_outputs_to_box(new_state)}),
-            //State::Detached => new_state,
-            //State::End => Box::new(State::End)
-        //}
-    //}
 }
 
--- src/nfa/spec.rs
+++ src/nfa/spec.rs
@@ -1,49 +1,10 @@
 use ::expr::Expr;
-use super::*;
+use super::{State};
 
 #[test]
 fn build_single() {
     let nfa = State::from_expr(&Expr::Single('a'));
 
     assert_eq!(State::state('a', State::End), nfa);
-}
-
-#[test]
-fn build_sequence() {
-    let expr = &Expr::sequence(Expr::Single('a'), Expr::Single('b'));
-
-    assert_eq!(State::state('a', State::state('b', State::End)),
-               State::from_expr(expr));
-}
-
-#[test]
-fn build_or() {
-    let expr = &Expr::or(Expr::Single('a'), Expr::Single('b'));
-
-    assert_eq!(State::split(State::state('a', State::End),
-                            State::state('b', State::End)),
-               State::from_expr(expr));
-}
-
-#[test]
-fn build_optional() {
-    let expr = &Expr::optional(Expr::Single('a'));
-
-    assert_eq!(State::split(State::state('a', State::End),
-                            State::End),
-               State::from_expr(expr));
-}
-
-#[test]
-#[ignore]
-fn build_zero_or_more() {
-    let expr = &Expr::zero_or_more(Expr::Single('a'));
-
-    let mut expected_state = State::split(State::state('a', State::Detached),
-                                          State::End);
-    expected_state = State::split(State::State{edge: 'a', out: Box::new(expected_state)},
-                                  State::End);
-
-    assert_eq!(expected_state, State::from_expr(expr));
 }
 
